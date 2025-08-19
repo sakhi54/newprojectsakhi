80711b0ca917b154ef49e9ae03ebbc14e538b8d8
sakhi54 <sakhiahmed54@gmail.com>
2025-08-19 14:02:16 +0530

    sak update

--- src/Sakhi.java
+++ src/Sakhi.java
@@ -1,4 +1,15 @@
 
+
 public class Sakhi {
 
+
+public void rate() {
+	// TODO Auto-generated method stub
+System.out.println("20k");
+}
+	// TODO Auto-generated method 
+	public static void main(String[] args) {
+
+	}
+Sakhi c=new Sakhi();
 }
